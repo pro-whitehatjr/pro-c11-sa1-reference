8920fe86ed3b97a9000530fd005403591629eb13
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-06-25 11:30:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,17 @@
-var marks = [40,35,45,38,40];
+var marks = [35,38,42,45,43,34,36,41,48,32];
+var sum = 0;
 
-function score_average()
-{
-  var sum = 0;
-  for(var i=0; i<marks.length; i=i+1)
-  {
-    
-  var sum = sum + marks[i];
-  
-  }
-  var avg = sum/marks.length;
-  console.log(avg);
 
 }
 
 function setup() {
   createCanvas(400,400);
-  score_average();
+  for(var i=0; i<marks.length; i=i+1)
+  {
+  var sum = sum + marks[i]; 
+  }
+  var average = sum/marks.length;
+  console.log(average);
 
 }
 
